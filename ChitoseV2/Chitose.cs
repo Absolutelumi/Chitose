dcb5c7ce35d0c240c0c47498e5bb290602290ca1
Jackson Saylor <jackson.saylor@yahoo.com>
2017-01-02 22:02:49 -0500

    Basic framework done; added "angry" command

---
+++ ChitoseV2/Chitose.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitoseV2
+{
+    class Chitose
+    {
+        DiscordClient client;
+        CommandService commands; 
+
+        public Chitose()
+        {
+
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            client.UsingCommands(input =>
+            {
+                input.PrefixChar = '>';
+                input.AllowMentionPrefix = true; 
+            });
+
+            commands = client.GetService<CommandService>();
+
+            commands.CreateCommand("angry").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("http://i.imgur.com/b6uQ2h3.jpg");
+            });
+
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect("MjY1MzU3OTQwNDU2Njg1NTc5.C0yg1w.f3gHb1eCeuMpAMqGGuc - 16Cm1wQ", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message); 
+        }
+    }
+}
--- ChitoseV2/Program.cs
+++ ChitoseV2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +12,7 @@ namespace ChitoseV2
     {
         static void Main(string[] args)
         {
-            client.MessageCreated += async (_, messageInfo) => {
-                if (messageInfo.Message.Content == "angry")
-                {
-                    await client.SendMessage(messageInfo.Message.ChannelID, "online url of image");
-                }
-            };
+            Chitose bot = new ChitoseV2.Chitose(); 
         }
     }
 }
