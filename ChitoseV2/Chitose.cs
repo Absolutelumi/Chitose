2c8d8e25a29c11e7a3142db3a5eb1d53fb852e5f
Jackson Saylor <jackson.saylor@yahoo.com>
2017-01-06 17:40:19 -0500

    New commands and such

--- ChitoseV2/Chitose.cs
+++ ChitoseV2/Chitose.cs
@@ -20,7 +20,7 @@ namespace ChitoseV2
 
             System.IO.StreamReader filereader = new System.IO.StreamReader("C:\\Users\\Scott\\Desktop\\BOT\\Chitose.txt");
 
-            string line = filereader.ReadLine(); 
+            string line = filereader.ReadLine();
 
             client = new DiscordClient(input =>
             {
@@ -46,9 +46,12 @@ namespace ChitoseV2
                 commands.CreateCommand(command[0]).Do(async (e) =>
                 {
                     await e.Channel.SendMessage(urls[random.Next(urls.Length)]);
+                    await e.Message.Delete();
                 });
                 line = filereader.ReadLine();
             }
+
+            Console.Clear(); 
 
             client.UserJoined += async (s, e) =>
             {
@@ -59,21 +62,57 @@ namespace ChitoseV2
                 await channel.SendMessage(string.Format("@everyone {0} has joined the server!", user.Name));
             };
 
+            client.UserLeft += async (s, e) =>
+            {
+                var channel = e.Server.FindChannels("announcements").FirstOrDefault();
+
+                var user = e.User;
+
+                await channel.SendMessage(string.Format("@everyone {0} has left the server.", user.Name)); 
+            };
+
+            client.UserBanned += async (s, e) =>
+            {
+                var channel = e.Server.FindChannels("announcements").FirstOrDefault();
+
+                await channel.SendMessage(string.Format("@everyone {0} has been banned from the server.", e.User.Name));
+            };
+
+            commands.CreateCommand("myrole").Do(async (e) =>
+            {
+                var role = string.Join(" , ", e.User.Roles); 
+
+                await e.Channel.SendMessage(string.Format("```Your roles are: {0} ```", role)); 
+            });
+
+            commands.CreateCommand("myav").Do(async (e) =>
+            {
+                await e.Channel.SendMessage(string.Format("{0}'s avatar is:  {1}", e.User.Mention, e.User.AvatarUrl));
+            });
+
+            commands.CreateCommand("triggered").Parameter("mention").Do(async (e) =>
+            {
+                await e.Message.Delete(); 
+                await e.Channel.SendMessage(string.Format("元気ね{0}くん。いい事あったかい？", e.GetArg("mention")));
+            });
+
+            commands.CreateCommand("osu").Parameter("user").Do(async (e) =>
+            {
+                await e.Channel.SendMessage(string.Format("http://lemmmy.pw/osusig/sig.php?colour=pink&uname={0}&pp=1&countryrank", e.GetArg("user")));
+            });
+
             commands.CreateCommand("help").Do(async (e) =>
             {
-                await e.Channel.SendMessage("Prefix   :   ' > '");
+                await e.Channel.SendMessage(string.Format("```{0}Prefix : '>' \n Chitose reaction picture commands : \n angry  - happy - thinking \n disappointed - annoyed - hopeful \n shocked```", e.User.Mention));
 
-                await e.Channel.SendMessage("Chitose reaction picture commands :");
-
-                await e.Channel.SendMessage(":angry: angry  -  :smile: happy  -  :thinking: thinking");
-                await e.Channel.SendMessage(":disappointed: disappointed  -  :frowning: annoyed  -  :smiley: hopeful");
-                await e.Channel.SendMessage(":exclamation: shocked"); 
+                await e.Channel.SendMessage("```Commands : 'myrole' , 'myav' , 'osu (user)' ```");
             });
 
             client.ExecuteAndWait(async () =>
             {
-                await client.Connect("MjY1MzU3OTQwNDU2Njg1NTc5.C0yg1w.f3gHb1eCeuMpAMqGGuc - 16Cm1wQ", TokenType.Bot);
+                await client.Connect("MjY1MzU3OTQwNDU2Njg1NTc5.C08iSQ.0JuccBwAn2mYftmvgNdygJyIK-w", TokenType.Bot);
             });
+
         }
 
         private void Log(object sender, LogMessageEventArgs e)
